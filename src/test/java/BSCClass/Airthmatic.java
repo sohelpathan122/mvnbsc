45d16f1fe08b983eb149eba5cb27a8576c736eb0
sohelpathan122 <sohelpathan5110@gmail.com>
2024-09-11 15:33:15 -0500

    Add Pr

---
+++ src/test/java/BSCClass/Airthmatic.java
@@ -0,0 +1,13 @@
+package BSCClass;
+
+import org.testng.annotations.Test;
+
+public class Airthmatic {
+  @Test
+  public void Plus() 
+  {
+	  int a=20;
+	  int b=20;
+	  System.out.println(a>b);
+  }
+}
